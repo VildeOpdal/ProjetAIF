1c66838d2408c9255ce7e755f6572ffb6328569e
LauKV <159470601+LauKV@users.noreply.github.com>
2024-11-21 09:07:23 +0100

    Update movies_genre_api.py
    update weight path

--- movies_genre_api.py
+++ movies_genre_api.py
@@ -13,7 +13,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 app = Flask(__name__)
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--model_path', type=str, default = 'models/Movies.pth', help='model path')
+parser.add_argument('--model_path', type=str, default = 'model_resnet50.pth', help='model path')
 args = parser.parse_args()
 model_path = args.model_path
 
