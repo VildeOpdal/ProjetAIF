26f143f34049679897f542164a73b153ee25ce7d
LauKV <159470601+LauKV@users.noreply.github.com>
2025-01-09 09:19:55 +0100

    Update movies_genre_api.py

--- movies_genre_api.py
+++ movies_genre_api.py
@@ -40,7 +40,7 @@ genre_labels = [
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])  # Standard for ResNet50
+    transforms.Normalize([0.5], [0.5])  # Standard for ResNet50
 ])
 
 # Root endpoint
